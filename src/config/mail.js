9a613e5cc678d49850b3808ea9f9eebafd01277f
Daniel Felizardo <daniel.david772@gmail.com>
2019-06-27 01:22:06 -0300

    configuration for send email

---
+++ src/config/mail.js
@@ -0,0 +1,25 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'aa4dbdad796932',
+    pass: 'febfa496279043',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
+
+/** Online
+ *
+ * Amazon SES
+ * Mailgun
+ * Spakpost
+ * Mandril(Mailchimp)
+ * Gmail
+ *
+ * Dezenvolvimento
+ *
+ * Mailtrap(Dev)
+ */
