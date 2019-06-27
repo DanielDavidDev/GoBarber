c16e0a8e8880394f066ff9ca881ec606eb5cba96
Daniel Felizardo <daniel.david772@gmail.com>
2019-06-27 05:02:32 -0300

    Add variables ammbient for aplication

--- src/config/mail.js
+++ src/config/mail.js
@@ -1,10 +1,10 @@
 export default {
-  host: 'smtp.mailtrap.io',
-  port: 2525,
+  host: process.env.MAIL_HOST,
+  port: process.env.MAIL_PORT,
   secure: false,
   auth: {
-    user: 'aa4dbdad796932',
-    pass: 'febfa496279043',
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
   },
   default: {
     from: 'Equipe GoBarber <noreply@gobarber.com>',
--- src/config/sentry.js
+++ src/config/sentry.js
@@ -1,3 +1,3 @@
 export default {
-  dsn: 'https://b2b562b57172486dbc51baa5a6459c00@sentry.io/1491522',
+  dsn: process.env.SENTRY,
 };
